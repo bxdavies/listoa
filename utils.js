15febda0efb96acdb31e774d9a45b16c1a9be0d7
Ben Davies <ben@daviesfamily.uk>
2022-01-05 16:59:53 +0000

    Fix

--- utils.js
+++ utils.js
@@ -1,10 +1,8 @@
 import { exportToJson } from './common/js/idb-backup-and-restore.js'
-function test(){
-    exportToJson(database)
-        .then(result => {
-            console.log('Exported JSON string:', result)
-        })
-        .catch(error => {
-            console.error('Something went wrong during export:', error)
-        })
-}
+exportToJson(database)
+    .then(result => {
+        console.log('Exported JSON string:', result)
+    })
+    .catch(error => {
+        console.error('Something went wrong during export:', error)
+    })
